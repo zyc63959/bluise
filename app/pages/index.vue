c93e9a4b20c9cc5dfae601ec6fa2a0f0faecafa7
zyc63959 <51981348+zyc63959@users.noreply.github.com>
2024-02-23 12:55:08 +0800

    33

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
+        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" style="visibility: hidden;"/>
 
         <div class="mb-12 xl:mb-0">
           <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
